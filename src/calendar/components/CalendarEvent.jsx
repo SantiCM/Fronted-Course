f15dc3f9b48bfaeeb284f13331500b6ad7df48d1
Santiago Melo <santiagocano15cr7@gmail.com>
2023-11-09 14:33:21 -0600

    "comentarios en CalendarEvent"

--- src/calendar/components/CalendarEvent.jsx
+++ src/calendar/components/CalendarEvent.jsx
@@ -1,8 +1,11 @@
-
+// Mandamos una constante que recibe la desestructturacion del evento
 export const CalendarEvent = ({event}) => {
     
+    // constante de desustructaracion de el title y el user
     const {title, user} = event
 
+    // Mostramos el titulo 
+    // Y mostaramos el nombre del usuario que se registro
     return (
 
         <>
